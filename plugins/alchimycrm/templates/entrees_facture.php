410b0ca88d6df722323981aea77e890efaa7c350
Paul <paul@agence-alchimy.fr>
2023-11-09 11:21:13 +0100

    [INVOICE] fixed product number display; fixed separators

--- plugins/alchimycrm/templates/entrees_facture.php
+++ plugins/alchimycrm/templates/entrees_facture.php
@@ -1,4 +1,4 @@
-<page backtop="30mm" backbottom="20mm" backleft="29mm" backright="29mm">
+<page backtop="20mm" backbottom="20mm" backleft="15mm" backright="15mm">
     <page_header>
         <div class="header">
             <hr style="border-width:0.1pt; margin-top:10pt; " />
@@ -16,36 +16,50 @@
             <p class="light" style="font-size: 6pt; text-align: center"><img src="medias/logoA.png" alt="" width="25"></p>
         </div>
     </page_footer>
-    <table class="contenu" style="width:100%; font-size: 9pt">
-            <tr>
-                <td>
-                    <h2 style="margin-bottom: 50pt; ">Détails des<br>prestations réalisées</h2>
-                </td>
-            </tr>
+    <div class="contenu">
+        <table class="entry"  style="width: 500px; table-layout: atuo; border: 0; padding-bottom: 10px">
+                <tr >
+                    <td>
+                        <h2 style="margin-bottom: 50pt; ">Détails des<br>prestations réalisées</h2>
+                    </td>
+                </tr>
 
-            <?php $num = count($services); foreach($services as $k=>$service): ?>
-            <tr>
-                <td style="width:85%; text-transform: uppercase;padding: 10pt 0; ">
-                    <?php 
-                    echo $service['titre']; 
+                <?php $num = count($services); foreach($services as $k=>$service): ?>
+                <tr style="border-bottom: 1px solid #a1a1a0; margin-bottom: 10px;">
+                    <td style=" width: 20%">
+                        <h3 ><?php echo wordwrap($service['titre'], 40, '<br/>', true); ?></h3>
+                    </td>
+                    <td  style="width: 10%;">
+                    <?php
+                    $n = 1;
+                    if($service['nombre'] > 1):
                     ?>
-                    <em style="text-transform: lowercase; white-space: nowrap;">
-                        <?php 
-                        $nombre_string = $service['nombre']  > 1 ? " ( x".$service['nombre']." )" : '';
-                        echo $nombre_string;
-                        ?>
-                    </em>
-                </td>
-                <td style="width:15%; text-align: right; padding: 10pt 0 "><?php echo (!empty($service['tarif'])) ?  str_replace(',00', '',number_format($service['tarif'], 2, ',', '.')).' &euro; HT' : 'OFFERT'; ?></td>
-            </tr>
-            <?php if($k<$num-1): ?>
-            <tr>
-                <td colspan="2"><hr style="border:0; border-bottom: 0.1pt solid #999;"></td>
-            </tr>
-            <?php endif; ?>
-            <?php endforeach; ?>
-            <tr style="width:100%;">
-                <td colspan="2"><hr style="border:0; border-bottom: 0.1pt solid #999;"></td>
-            </tr>
-    </table>
+                        <h3>x <?php echo $service['nombre']; ?></h3>
+                    <?php
+                    $n = $service['nombre'];
+                    endif;
+                    ?>
+                    </td>
+                    <td style="width: 23%">
+                        <?php if(empty($service['tarif'])) : ?>
+                        <h3 style="text-align: right; width: 100%">OFFERT</h3>
+                        <?php else: ?>
+                        <h3 style="text-align: right; ; width: 100%"><?php echo str_replace(',00', '',number_format($n*$service['tarif'], 2, ',', '.')); ?> &euro; HT</h3>
+                        <?php endif; ?>
+                    </td>
+                </tr>
+                <?php if($k<$num-1): ?>
+                    <tr style="width:100%;">
+                        <td colspan="3">
+                            <div style="height: 1px; background: #a1a1a0; margin-bottom: 10px;"></div>
+                        </td>
+                        <td style="width:33%;" colspan="1">
+                            <div style="height: 1px; background: #a1a1a0; margin-bottom: 10px; margin-left: -10px"></div>
+                        </td>
+                    </tr>
+                <?php endif; ?>
+                <?php endforeach; ?>
+        </table>
+         
+    </div>
 </page>
